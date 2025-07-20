16c2bcfe54c9ba99de67bb122a3a482e15408906
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-07-20 16:12:57 +0530

    integrate CORS

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from routers import upload, chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:3000", "https://your-frontend-url.com"]
+    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(upload.router)
 app.include_router(chat.router)
